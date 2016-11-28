964b61e610c263431a94fbdd760a35a30f08f6d5
Jaeyoung Choi <hideyf@gmail.com>
2016-11-29 08:17:29 +0900

    Add new project

---
+++ AppUIKit/AppUIKit.h
@@ -0,0 +1,19 @@
+//
+//  AppUIKit.h
+//  AppUIKit
+//
+//  Created by ricky on 2016. 11. 29..
+//  Copyright © 2016년 appcid. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AppUIKit.
+FOUNDATION_EXPORT double AppUIKitVersionNumber;
+
+//! Project version string for AppUIKit.
+FOUNDATION_EXPORT const unsigned char AppUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppUIKit/PublicHeader.h>
+
+
