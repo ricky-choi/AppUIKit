6780764c68b5188cdcdc555cd94e1568a9d9dd0d
Jae Young Choi <hideyf@gmail.com>
2017-01-19 01:05:41 +0900

    modify AUIControlState’s rawValue

--- AppUIKit/AUIControl.swift
+++ AppUIKit/AUIControl.swift
@@ -20,17 +20,17 @@ public struct AUIControlState : OptionSet {
     }
     
     
-    public static var normal: AUIControlState { return AUIControlState(rawValue: 0b1) }
+    public static var normal: AUIControlState { return AUIControlState(rawValue: 0) }
     
-    public static var highlighted: AUIControlState { return AUIControlState(rawValue: 0b10) } // used when UIControl isHighlighted is set
+    public static var highlighted: AUIControlState { return AUIControlState(rawValue: 0b1) } // used when UIControl isHighlighted is set
     
-    public static var disabled: AUIControlState { return AUIControlState(rawValue: 0b100) }
+    public static var disabled: AUIControlState { return AUIControlState(rawValue: 0b10) }
     
-    public static var selected: AUIControlState { return AUIControlState(rawValue: 0b1000) } // flag usable by app (see below)
+    public static var selected: AUIControlState { return AUIControlState(rawValue: 0b100) } // flag usable by app (see below)
     
-    public static var focused: AUIControlState { return AUIControlState(rawValue: 0b10000) } // Applicable only when the screen supports focus
+    public static var focused: AUIControlState { return AUIControlState(rawValue: 0b1000) } // Applicable only when the screen supports focus
     
-    public static var application: AUIControlState { return AUIControlState(rawValue: 0b100000) } // additional flags available for application use
+    public static var application: AUIControlState { return AUIControlState(rawValue: 0b111111110000000000000000) } // additional flags available for application use
     
-    public static var reserved: AUIControlState { return AUIControlState(rawValue: 0b1000000) } // flags reserved for internal framework use
+    public static var reserved: AUIControlState { return AUIControlState(rawValue: 0b11111111000000000000000000000000) } // flags reserved for internal framework use
 }
